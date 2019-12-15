126873ca25a6c55bf105e1aa12fa32d289062c7d
Chayanne <chayannepaulino@gmail.com>
2019-12-15 14:47:45 -0500

    atm constructor add

--- src/Atm/Atm.java
+++ src/Atm/Atm.java
@@ -1,4 +1,12 @@
 package Atm;
 
-public class Atm {
+public class Atm extends Bank {
+
+    private boolean validate;
+
+
+    /* constructor */
+    public Atm(String card_num, String card_pin) {
+        super(card_num, card_pin);
+    }
 }
