de861ba505446810687cbb1b803f7b787766d097
Chayanne <chayannepaulino@gmail.com>
2019-12-16 21:27:47 -0500

    Connecting and inserting data to DB

--- src/Atm/Atm.java
+++ src/Atm/Atm.java
@@ -4,9 +4,18 @@ public class Atm extends Bank {
 
     private boolean validate;
 
-
     /* constructor */
     public Atm(String card_num, String card_pin) {
         super(card_num, card_pin);
     }
+
+    public static void main (String [] args) {
+
+        Bank bankTLC = new Bank("1002", "5263");
+
+        bankTLC.insertDB();
+
+    }
+
+
 }
--- src/Atm/Bank.java
+++ src/Atm/Bank.java
@@ -1,11 +1,16 @@
 package Atm;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class Bank {
 
 
     private boolean validateUser;
     private String card_num;
     private String card_pin;
+
 
     /* Getters */
     public void setValidateUser(boolean validateUser) {
@@ -42,5 +47,38 @@ public class Bank {
     }
 
 
-    /*Each method will be connecting the the DB to verifiy the card  and get user account info*/
+    //Each method will be connecting the the DB to verifiy the card  and get user account info
+
+    /* Inserting data to DB this method is just for easy data entry!
+    be wrap in a group comment for when the application is running */
+    public void insertDB() {
+
+        Connection conn = null;
+        Statement stmt = null;
+        String url = "jdbc:sqlite:Bank_DB.db";
+
+        try {
+
+            conn = DriverManager.getConnection(url);
+            stmt = conn.createStatement();
+            conn.setAutoCommit(false);
+
+            System.out.println("Database connection successfully");
+
+            String sql = "INSERT INTO Customer (customer_name,  customer_dob, card_number, card_pin)\n" +
+                    "VALUES ('Angel vargas' , '02/03/1980', 1001, 2536)";
+
+            stmt.execute(sql);
+            stmt.close();
+            conn.commit();
+            conn.close();
+
+        } catch (Exception e ) {
+
+            System.err.println(e.getMessage());
+        }
+
+        System.out.println("Records created successfully");
+    }
+
 }
