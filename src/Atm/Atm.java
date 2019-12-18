1b350c55b33628a0531621742a030e5025de0a55
Chayanne <chayannepaulino@gmail.com>
2019-12-17 22:52:47 -0500

    two method completed: DB connection and valCardandPin

--- src/Atm/Atm.java
+++ src/Atm/Atm.java
@@ -11,7 +11,7 @@ public class Atm extends Bank {
 
         Bank bankTLC = new Bank("1001", "1234");
 
-        final boolean validate = bankTLC.validateInfo(bankTLC.getCard_num(), bankTLC.getCard_pin());
+        boolean validate = bankTLC.valCardandPin(bankTLC.getCard_num(), bankTLC.getCard_pin());
 
         if (validate) {
             System.out.print("Your card Information exists in our Database!");
--- src/Atm/Bank.java
+++ src/Atm/Bank.java
@@ -1,9 +1,6 @@
 package Atm;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Bank {
 
@@ -55,80 +52,32 @@ public class Bank {
     }
 
 
-    //Each method will be connecting the the DB to verifiy the card and user account info
+    /* The ValCardandPin method check if the card number and pin enter by customer exists in the database */
+    public final Boolean valCardandPin(String card_num, String card_pin) {
 
-    /* Inserting data to DB this method is just for easy data entry!
-    be wrap in a group comment for when the application is running
-    public void insertDB() {
-
-        Connection conn = null;
-        Statement stmt = null;
-        String url = "jdbc:sqlite:Bank_DB.db";
-
-        try {
-
-            conn = DriverManager.getConnection(url);
-            stmt = conn.createStatement();
-            conn.setAutoCommit(false);
-
-            System.out.println("Database connection successfully");
-
-            String sql = "INSERT INTO Customer (customer_name,  customer_dob, card_number, card_pin)\n" +
-                    "VALUES ('Angel vargas' , '02/03/1980', 1001, 2536)";
-
-            stmt.execute(sql);
-            stmt.close();
-            conn.commit();
-            conn.close();
-
-        } catch (Exception e ) {
-
-            System.err.println(e.getMessage());
-        }
-
-        System.out.println("Records created successfully");
-    }*/
-
-
-    public static Boolean validateInfo(String card_num, String card_pin) {
-
-        Connection conn = null;
-        Statement stmt = null;
-        ResultSet result = null;
         Boolean validate = null;
+        String sql = "SELECT count(a.card_number)  card_number,\n" +
+                "count(b.card_pin) as card_pin\n" +
+                "FROM  Customer a\n" +
+                "LEFT  JOIN Customer b\n" +
+                "on a.customer_id = b.customer_id\n" +
+                "where a.card_number = ? and b.card_pin = ?";
 
-        String url = "jdbc:sqlite:Bank_DB.db";
+        try (Connection conn = this.connection();
+             PreparedStatement pstmt  = conn.prepareStatement(sql)){
 
-        try {
-            conn = DriverManager.getConnection(url);
-            stmt = conn.createStatement();
-            conn.setAutoCommit(false);
+            /* Set the value */
+            pstmt.setString(1, card_num);
+            pstmt.setString(2,card_pin);
+            ResultSet rs  = pstmt.executeQuery();
 
-            String valCardquery = "SELECT COUNT(card_number)\n" +
-                    "FROM Customer\n" +
-                    "WHERE card_number = ?;";
+            /* getBoolean return true if the value from the query is 1 and 0 if the value is 0 */
+            boolean valcard= rs.getBoolean("card_number");
+            boolean valpin =rs.getBoolean("card_pin");
 
-            String valPinquery = "SELECT COUNT(card_pin)\n" +
-                    "FROM Customer\n" +
-                    "WHERE card_number = ?;";
 
-            Boolean valCard = stmt.execute(valCardquery);
-            Boolean valPin = stmt.execute(valPinquery);
-
-            stmt.execute(valCardquery);
-            stmt.execute(valPinquery);
-
-            if (valCard && valPin) {
-
-                validate = true;
-                stmt.close();
-                conn.commit();
-                conn.close();
-
-            } else {
-                System.out.println("Your Card number or Pin number are incorrect.");
-                System.out.println("Please try again!");
-            }
+            if (valcard && valpin){ validate = true;}
+            conn.close();
 
         } catch(Exception e ) {
 
